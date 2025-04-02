d9c7701f5b6cbf90ae0145e71fd9283cb1017927
adyHensem01 <45536898+adyHensem01@users.noreply.github.com>
2025-04-02 16:25:46 +0000

    #no1 fix npm run test ady

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom', // Use jsdom for React components
   transform: {
-    '^.+\\.tsx?$': 'ts-jest', // Use ts-jest for .ts and .tsx files
+    '^.+\\.tsx?$': 'ts-jest', // Transform TypeScript and JSX files
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock CSS imports
-    '\\.(svg|png|jpg|jpeg|gif|webp|avif|ico|bmp|tiff)$': '<rootDir>/tests/__mocks__/fileMock.js', // Mock static assets
   },
 };
